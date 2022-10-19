99a4366da4c43b29b019adff86715468acaf2de2
HongGunWoo <foxrain.gg@gmail.com>
2022-10-19 15:17:38 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20181270/w0702_explicit_intent/ImageActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.kumoh.s20181270.w0702_explicit_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
